b7c32269101ad352081ac39696012be21a1f3b64
Glitch (proxy-bot-dsc) <none>
2021-09-22 05:46:03 +0000

    fixed issue for channels with multiple webhooks

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ const token = process.env.TOKEN;
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
-const {relay,hide} = require("./config.json")
-let prefix = process.env.PREFIX;
-
+const { prefix, relay, hide, logs, whitelist } = require("./config.json");
+const { servers } = require("./whitelist.json");
+const uptime = Date.now();
 client.once("ready", () => {
   console.log("Ready!");
 });
@@ -24,24 +24,32 @@ client.once("ready", () => {
 client.on("messageCreate", async message => {
   if (message.author.bot) return;
   if (!message.content) return;
-  if (!message.content.startsWith(process.env.PREFIX)) return;
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  if (whitelist == true && !servers.includes(parseInt(message.guildId))) return;
+  // if (!message.content.startsWith(prefix) || message.author.bot) return;
 
   const args = message.content
     .slice(prefix.length)
     .trim()
     .split(" ");
   const commandName = args.shift().toLowerCase();
+  // proxy command
   if (commandName == "p" || commandName == "proxy" || commandName == "hide") {
     try {
       let webhook;
       const webhooks = await message.channel.fetchWebhooks();
       webhook = webhooks.first();
-
+      await webhooks.forEach(async hook => {
+        if (webhook.owner.id == client.user.id) return;
+        if (hook.owner.id == client.user.id) webhook = hook;
+      });
       if (!webhook) webhook = await message.channel.createWebhook("Proxy", {});
       let name = message.author.username;
       let av = message.author.avatarURL();
-      if(commandName == "hide" && hide == true){ name = "Someone"; av = undefined}
+      if (commandName == "hide" && hide == true) {
+        name = "Someone";
+        av = undefined;
+      }
       let payload = message.content
         .split(" ")
         .slice(1)
@@ -54,22 +62,34 @@ client.on("messageCreate", async message => {
         files: message.attachments
       });
       await message.delete();
+
+      if (logs) {
+        await client.channels.cache.get(logs).send({
+          content: `${message.author.username} with id ${message.author.id} proxied in ${message.channel}`
+        });
+      }
     } catch (err) {
       console.log(err);
     }
-  } else if (commandName == "help") {
+  }
+  // help command, totally useless
+  else if (commandName == "help") {
     try {
       message.channel.send({ content: "go fuck yourself" });
     } catch (err) {
       console.log(err);
     }
-  } else if (commandName == "upupdowndownleftrightleftrightba") {
+  }
+  //actual help command
+  else if (commandName == "upupdowndownleftrightleftrightba") {
     try {
-      message.channel.send({ content: "Kanomi code does exist" });
+      message.channel.send({ content: `use ${prefix}proxy or ${prefix}p` });
     } catch (err) {
       console.log(err);
     }
-  } else if (commandName == "ping") {
+  }
+  // ping command, kinda helpful
+  else if (commandName == "ping") {
     try {
       const embed = new MessageEmbed()
         .setColor("WHITE")
@@ -78,7 +98,7 @@ client.on("messageCreate", async message => {
           `Latency is ${Date.now() -
             message.createdTimestamp}ms. API Latency is ${Math.round(
             client.ws.ping
-          )}ms`
+          )}ms \nUptime:${Math.round((Date.now() - uptime) / 1000)} s`
         );
       message.channel.send({ embeds: [embed] });
     } catch (err) {
@@ -89,11 +109,10 @@ client.on("messageCreate", async message => {
 
 client.on("ready", () => {
   const activities = [
- //   ["", "PLAYING"],
- //   [" ", "LISTENING"],
+    //   ["", "PLAYING"],
+    //   [" ", "LISTENING"],
     ["your browser history", "WATCHING"],
     ["for your memes", "WATCHING"],
-    ["out for that reporter", "WATCHING"],
     ["your mom", "PLAYING"]
   ];
   setInterval(() => {
